bb67fe0f28201c7c07f88d0604c60bf7c9f51cd2
tairoroberto <tairoroberto@hotmail.com>
2016-12-04 21:16:59 -0200

    Update app

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -67,7 +67,9 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String forecast = (String) mForecastAdapter.getItem(i);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent mIntent = new Intent(getActivity(), DetailActivity.class);
+                mIntent.putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(mIntent);
             }
         });
 
